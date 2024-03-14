17ead8ef434fa78e9ebd187f5b366e7a8edbe10d
emiliantimofei <metimo1996@gmail.com>
2024-03-14 11:53:16 +0100

    failed still learn

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+let emptyArray = [];
+
+let sum = 0;
+while (sum < 50){
+    let newNum = parseInt(prompt('Inserisci un numero'));
+}
+
+
